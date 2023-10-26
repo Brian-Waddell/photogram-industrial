847ef15d75904a40bca9719ff99b8fcbb8d7a442
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-10-26 13:59:27 +0000

    replace the photo code in show with partial helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
     @user = User.find_by!(username: params.fetch(:username))
 
   end 
-
+  
+  def liked
+    @user = User.find_by!(username: params.fetch(:username))
+  end
   
 end 
